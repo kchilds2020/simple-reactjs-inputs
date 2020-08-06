f8542432599a90b04b787297facdf52d93e27879
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-08-06 15:51:23 -0500

    Updates

---
+++ precache-manifest.ba2fa95a504f2563758673752a384bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffd04ad70ad8b2e0bec10825d0de815",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8795dd4a173b03ddf3d",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "3232f41bec9c4300f936",
+    "url": "./static/js/2.60cab5b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.60cab5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8795dd4a173b03ddf3d",
+    "url": "./static/js/main.80e7010d.chunk.js"
+  },
+  {
+    "revision": "257db4c94232a8b92aa2",
+    "url": "./static/js/runtime-main.1afe88a3.js"
+  }
+]);
